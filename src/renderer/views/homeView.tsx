ab0f1d0ca72933163ee7d23ce22cb9ded9eb3aaf
script rootie <script.rootie@pm.me>
2021-05-10 07:55:27 +0200

    Fix play btn

--- src/renderer/views/homeView.tsx
+++ src/renderer/views/homeView.tsx
@@ -21,7 +21,7 @@ class HomeView extends Component<HomeViewProps, HomeViewState> {
                 <HomeViewFeed steamNews={this.props.steamNews}
                     onRefresh={this.props.onFeedRefresh} />
                 <footer id="homeview__footer">
-                    <button id="homeview__footer-play-btn" className="green-btn" onClick={kerbolAPI.gameManager.runInstance(this.props.instanceId)}>Play</button>
+                    <button id="homeview__footer-play-btn" className="green-btn" onClick={() => kerbolAPI.gameManager.runInstance(this.props.instanceId)}>Play</button>
                 </footer>
             </React.Fragment>
         );
